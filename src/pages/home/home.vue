1407986be40a1f425e3a811111fbea5af54d8eb7
1446450047 <1446450047@qq.com>
2022-10-20 23:24:45 +0800

    添加heaer文件

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
+        <Header />
         <h1>home</h1>
     </div>
 </template>
 
 <script setup lang="ts" name="home">
+import Header from '@/components/header/header.vue';
 
 </script>
