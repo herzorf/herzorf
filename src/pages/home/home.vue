0898c338ba048174b837d54cbd6f64ec55d3eb89
1446450047 <1446450047@qq.com>
2022-10-27 22:51:25 +0800

    提取数据

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -27,45 +27,11 @@
 import Header from '@/components/header/header.vue';
 import PersonalDataItem from './components/personal-data-item/personal-data-item.vue';
 import avatar from "@/assets/avatar.jpg"
+import {leftInfo,rightInfo} from "./util"
 import { ref } from 'vue';
 const personalData = ref(null)
 
-const leftInfo = [
-    {
-        title:"姓名",
-        info:"何中峰(Herzorf)",
-    },
-    {
-        title:"人生目标",
-        info:"身体自由，思想自由，财务自由",
-    },
-    {
-        title:"目前具备的技能",
-        info:"web开发",
-    },
-    {
-        title:"以后将要具备的技能",
-        info:"全栈开发",
-    }
-]
-const rightInfo = [
-    {
-        title:"工作经验",
-        info:"2022.8.1---至今",
-    },
-    {
-        title:"年龄",
-        info:"23",
-    },
-    {
-        title:"邮箱",
-        info:"1446450047@qq.com",
-    },
-    {
-        title:"目前居住地",
-        info:"上海",
-    }
-]
+
 
 </script>
 <style scoped lang="scss">
---
+++ src/pages/home/util.ts
@@ -0,0 +1,38 @@
+const leftInfo = [
+    {
+        title: "姓名",
+        info: "何中峰(Herzorf)",
+    },
+    {
+        title: "人生目标",
+        info: "身体自由，思想自由，财务自由",
+    },
+    {
+        title: "目前具备的技能",
+        info: "web开发",
+    },
+    {
+        title: "以后将要具备的技能",
+        info: "全栈开发",
+    }
+]
+const rightInfo = [
+    {
+        title: "工作经验",
+        info: "2022.8.1---至今",
+    },
+    {
+        title: "年龄",
+        info: "23",
+    },
+    {
+        title: "邮箱",
+        info: "1446450047@qq.com",
+    },
+    {
+        title: "目前居住地",
+        info: "上海",
+    }
+]
+
+export { leftInfo, rightInfo }
