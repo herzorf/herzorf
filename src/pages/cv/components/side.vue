ba92267cb116bd2f9c848f6a6ed8a19e51cff052
1446450047 <1446450047@qq.com>
2022-11-13 20:42:09 +0800

    重构代码

---
+++ src/pages/cv/components/side.vue
@@ -0,0 +1,64 @@
+<template>
+     <aside>
+      <div class="avatar">
+        <img :src="avatar" width="160" height="160" alt="" />
+        <h2>何中峰</h2>
+        <p>应聘前端工程师</p>
+      </div>
+      <ul class="info">
+        <li><Svg class="age"></Svg> <span>1999/10/25</span></li>
+        <li><Svg class="address"></Svg> <span>上海市徐汇区</span></li>
+        <li><Svg class="mobile"></Svg> <span>17384888339</span></li>
+        <li><Svg class="email"></Svg> <span>herzorf@icloud.com</span></li>
+        <li><Svg class="weixin"></Svg> <span>hzf1446450047</span></li>
+        <li><Svg class="colleague"></Svg> <span>西北大学(211)</span></li>
+        <li><Svg class="school"></Svg> <span>2022/7毕业</span></li>
+        <li><Svg class="major"></Svg> <span>软件工程专业</span></li>
+      </ul>
+    </aside>
+</template>
+<script setup lang="ts">
+import avatar from "@/assets/images/avatar.jpg";
+import Svg from "@/components/svg/svg.vue";
+</script>
+<style lang="scss">
+aside {
+    width: 30%;
+    background-color: #ececec;
+    padding: 1em;
+
+    .avatar {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        h2 {
+            border-bottom: 1px solid black;
+            padding-bottom: 0.25em;
+            margin-bottom: 0.5em;
+            font-weight: 400;
+        }
+    }
+
+    .info {
+        margin-top: 4em;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+
+        li {
+            width: 100%;
+            padding: 6px 0;
+            border-bottom: 1px solid black;
+
+            span {
+                border-left: 1px solid black;
+                margin-bottom: 8px;
+                padding-left: 8px;
+            }
+        }
+    }
+}
+</style>
--- src/pages/cv/cv.vue
+++ src/pages/cv/cv.vue
@@ -1,22 +1,6 @@
 <template>
   <article>
-    <aside>
-      <div class="avatar">
-        <img :src="avatar" width="160" height="160" alt="" />
-        <h2>何中峰</h2>
-        <p>应聘前端工程师</p>
-      </div>
-      <ul class="info">
-        <li><Svg class="age"></Svg> <span>1999/10/25</span></li>
-        <li><Svg class="address"></Svg> <span>上海市徐汇区</span></li>
-        <li><Svg class="mobile"></Svg> <span>17384888339</span></li>
-        <li><Svg class="email"></Svg> <span>herzorf@icloud.com</span></li>
-        <li><Svg class="weixin"></Svg> <span>hzf1446450047</span></li>
-        <li><Svg class="colleague"></Svg> <span>西北大学(211)</span></li>
-        <li><Svg class="school"></Svg> <span>2022/7毕业</span></li>
-        <li><Svg class="major"></Svg> <span>软件工程专业</span></li>
-      </ul>
-    </aside>
+    <Side/>
     <main>
       <MySkill />
       <MyWorkExperience/>
@@ -28,14 +12,14 @@
   </article>
 </template>
 <script setup lang="ts">
-import avatar from "@/assets/images/avatar.jpg";
-import Svg from "@/components/svg/svg.vue";
+
 import MyWorkExperience from "./components/my-work-experience.vue";
 import MySkill from "./components/my-skill.vue";
 import MyProjectExperience from "./components/my-project-experience.vue";
 import OtherProject from "./components/other-project.vue";
 import OtherLink from "./components/other-link.vue";
 import AboutMe from "./components/about-me.vue";
+import Side from "./components/side.vue";
 </script>
 <style lang="scss" scope>
 a {
@@ -50,46 +34,6 @@ article {
     margin: 0 auto;
     font-family: initial;
     padding: 0 1em;
-
-    aside {
-        width: 30%;
-        background-color: #ececec;
-        padding: 1em;
-
-        .avatar {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-
-            h2 {
-                border-bottom: 1px solid black;
-                padding-bottom: 0.25em;
-                margin-bottom: 0.5em;
-                font-weight: 400;
-            }
-        }
-
-        .info {
-            margin-top: 4em;
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-start;
-            align-items: flex-start;
-
-            li {
-                width: 100%;
-                padding: 6px 0;
-                border-bottom: 1px solid black;
-
-                span {
-                    border-left: 1px solid black;
-                    margin-bottom: 8px;
-                    padding-left: 8px;
-                }
-            }
-        }
-    }
 
     main {
         margin: 20px;
