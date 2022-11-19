c41bd70d41ecae8f85debecb9cfe2e95c3e259bc
1446450047 <1446450047@qq.com>
2022-11-19 09:50:29 +0800

    使用pinia

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import "./reset.css"
 import routes from "./routes"
 import { createRouter, createWebHashHistory } from "vue-router"
 import "./assets/font/fontcss"
+import { createPinia } from 'pinia'
 
+const pinia = createPinia()
 const router = createRouter({
     history: createWebHashHistory(),
     routes,
 })
-
 const app = createApp(App)
+app.use(pinia)
+
 app.use(router)
 app.mount('#app')
