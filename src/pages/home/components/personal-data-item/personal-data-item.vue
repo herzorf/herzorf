79e0bfa5cb30019780e372aa279c50cd0e5015d5
1446450047 <1446450047@qq.com>
2022-11-24 22:51:15 +0800

    feat: 首页手机端适配

--- src/pages/home/components/personal-data-item/personal-data-item.vue
+++ src/pages/home/components/personal-data-item/personal-data-item.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="itemWrapper">
-        <div class="title">
-            <slot name="title"></slot>
-        </div>
-        <div class="info">
-            <slot name="info"></slot>
-        </div>
+  <div class="itemWrapper">
+    <div class="title">
+      <slot name="title"></slot>
     </div>
+    <div class="info">
+      <slot name="info"></slot>
+    </div>
+  </div>
 </template>
 <script lang="ts">
-export default {
-    
-}
+  export default {};
 </script>
 <style lang="scss">
-    .itemWrapper{
-        .title{
-            font-size: 14px;
-            color: #999;
-        }
-        .info{
-            font-size: 24px;
-            margin: 18px 0;
-        }
-        
+  .itemWrapper {
+    .title {
+      font-size: 14px;
+      color: #999;
     }
+    .info {
+      font-size: 24px;
+      margin: 18px 0;
+      @media (max-width: 576px) {
+        font-size: 18px;
+      }
+    }
+  }
 </style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -3,6 +3,9 @@
     <h1>Herzorf</h1>
     <h1>一个web开发工程师</h1>
   </section>
+  <div class="mobileAvatar">
+    <img :src="me" width="300" alt="自拍" />
+  </div>
   <section class="personalData" ref="personalData">
     <div class="left">
       <PersonalDataItem v-for="item in leftInfo">
@@ -11,7 +14,9 @@
       </PersonalDataItem>
     </div>
     <div class="center">
-      <img :src="avatar" width="160" alt="自拍" />
+      <div class="border">
+        <img :src="me" height="600" alt="自拍" />
+      </div>
     </div>
     <div class="right">
       <PersonalDataItem v-for="item in rightInfo">
@@ -23,7 +28,7 @@
   <section class="advantage" ref="advantage">
     <h1>我的技能</h1>
     <div class="advantageContent">
-      <div class="advantageItem"  v-for="item in advantageItem">
+      <div class="advantageItem" v-for="item in advantageItem">
         <div class="skill">
           <Svg :class="item.class"></Svg>
           <span>{{ item.percent }}</span>
@@ -38,7 +43,7 @@
       <div class="education">
         <span class="title">{{ "2022/7毕业" }}</span>
         <h3>西北大学</h3>
-        <span class="info">{{ "软件工程" }}</span>
+        <span class="info">{{ "软件工程（本科）" }}</span>
       </div>
       <div class="workExperience">
         <span class="title">{{ "2022/8 - 至今" }}</span>
@@ -51,16 +56,12 @@
     <h1>我的最新项目</h1>
     <div class="showProjects">
       <div class="projectItem" @click="gotoTop">
-          <img :src="logo" >
-          <p>个人空间</p>
-          <p>Vue3 + TS </p>
+        <img :src="logo" />
+        <p>个人空间</p>
+        <p>Vue3 + TS</p>
       </div>
-      <div class="projectItem">
-
-      </div>
-      <div class="projectItem">
-
-      </div>
+      <div class="projectItem"></div>
+      <div class="projectItem"></div>
     </div>
   </section>
   <section class="whereIAm" ref="whereIAm">
@@ -73,12 +74,12 @@
       </div>
       <div class="socialItem">
         <a href="https://www.zhihu.com/people/hezhongfeng/posts">
-        <Svg class="zhihu"></Svg>
+          <Svg class="zhihu"></Svg>
         </a>
       </div>
       <div class="socialItem">
         <a href="https://juejin.cn/user/3122268755467566/posts">
-        <Svg class="juejin"></Svg>
+          <Svg class="juejin"></Svg>
         </a>
       </div>
       <div class="socialItem">
@@ -88,268 +89,314 @@
       </div>
     </div>
   </section>
-  <footer>  Copyright © 2022  Herzorf 版权所有 陕ICP备2022012657号</footer>
+  <footer>Copyright © 2022 Herzorf 版权所有 陕ICP备2022012657号</footer>
 </template>
 
 <script setup lang="ts" name="home">
-import Header from "@/components/header/header.vue";
-import PersonalDataItem from "./components/personal-data-item/personal-data-item.vue";
-import avatar from "@/assets/images/avatar.jpg";
-import { leftInfo, oberveApper, rightInfo, advantageItem } from "./util";
-import { onMounted, ref } from "vue";
-import logo from "@/assets/images/logo_transparent.png"
-import Svg from "../../components/svg/svg.vue";
-const personalData = ref<HTMLElement>();
-const advantage = ref<HTMLElement>();
-const project = ref<HTMLElement>();
-const experience = ref<HTMLElement>();
-const whereIAm = ref<HTMLElement>();
-const gotoTop = () => {
-  document.documentElement.scrollTop = 0
-}
-onMounted(() => {
-  oberveApper(personalData.value!);
-  oberveApper(advantage.value!);
-  oberveApper(project.value!);
-  oberveApper(experience.value!);
-  oberveApper(whereIAm.value!);
-});
+  import PersonalDataItem from "./components/personal-data-item/personal-data-item.vue";
+  import me from "@/assets/images/me.jpg";
+  import { leftInfo, oberveApper, rightInfo, advantageItem } from "./util";
+  import { onMounted, ref } from "vue";
+  import logo from "@/assets/images/logo_transparent.png";
+  import Svg from "../../components/svg/svg.vue";
+  const personalData = ref<HTMLElement>();
+  const advantage = ref<HTMLElement>();
+  const project = ref<HTMLElement>();
+  const experience = ref<HTMLElement>();
+  const whereIAm = ref<HTMLElement>();
+  const gotoTop = () => {
+    document.documentElement.scrollTop = 0;
+  };
+  onMounted(() => {
+    oberveApper(personalData.value!);
+    oberveApper(advantage.value!);
+    oberveApper(project.value!);
+    oberveApper(experience.value!);
+    oberveApper(whereIAm.value!);
+  });
 </script>
-<style scoped lang="scss">
-h1 {
-  font-size: 48px;
-  font-weight: lighter;
-}
-
-.personalInfo {
-  height: 60vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
+<style lang="scss" scoped>
   h1 {
-    font-weight: 100;
-    font-size: 64px;
-    margin-bottom: 10px;
-  }
-}
-
-.personalData {
-  min-height: 100vh;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  opacity: 0;
-  transition: 2.5s;
-  border-bottom: 1px solid black;
-
-  >div {
-    width: 20%;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: flex-start;
-
-    &.center {
-      flex-grow: 1;
+    font-size: 48px;
+    font-weight: lighter;
+    margin: 20px 0;
+    @media (max-width: 576px) {
+      font-size: 32px;
     }
   }
-}
+  .mobileAvatar {
+    display: none;
+    border: 1px solid #dfdfdf;
+    overflow: hidden;
+    padding: 10px;
+    border-radius: 50%;
+    img {
+      border-radius: 50%;
+    }
+    @media (max-width: 576px) {
+      display: block;
+      text-align: center;
+    }
+  }
+  .personalInfo {
+    height: 60vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
-.advantage {
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  opacity: 0;
-  transition: 2.5s;
-  border-bottom: 1px solid black;
+    h1 {
+      font-weight: 100;
+      font-size: 64px;
+      margin-bottom: 10px;
+      @media (max-width: 576px) {
+        font-size: 32px;
+      }
+    }
+  }
 
-  .advantageContent {
+  .personalData {
+    min-height: 100vh;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    width: 100%;
-    flex-wrap: wrap;
+    opacity: 0;
+    transition: 2.5s;
+    border-bottom: 1px solid black;
 
-    .advantageItem {
-      padding: 16px 0;
-      font-size: 50px;
-      width: 100px;
+    > div {
+      width: 20%;
+      height: 100vh;
       display: flex;
       flex-direction: column;
-      align-items: center;
       justify-content: space-around;
+      align-items: flex-start;
+      @media (max-width: 576px) {
+        width: 45%;
+      }
+      &.center {
+        flex-grow: 1;
+        align-items: center;
+        .border {
+          padding: 30px;
+          border: 1px solid #dfdfdf;
+          border-radius: 50%;
+          overflow: hidden;
+          img {
+            border-radius: 50%;
+          }
+        }
 
-      .skill {
-        font-size: 100px;
+        @media (max-width: 576px) {
+          display: none;
+          flex-grow: 0;
+        }
+      }
+    }
+  }
+
+  .advantage {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    opacity: 0;
+    transition: 2.5s;
+    border-bottom: 1px solid black;
+
+    .advantageContent {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      flex-wrap: wrap;
+
+      .advantageItem {
+        padding: 16px 0;
+        font-size: 50px;
+        width: 100px;
         display: flex;
         flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        margin: 0 30px;
+
+        .skill {
+          font-size: 100px;
+          display: flex;
+          flex-direction: column;
+
+          span {
+            margin: 18px 0;
+            font-size: 30px;
+            text-align: center;
+          }
+        }
 
         span {
-          margin: 18px 0;
-          font-size: 30px;
-          text-align: center;
-        }
-      }
-
-      span {
-        font-size: 16px;
-      }
-
-      &:hover {
-        svg {
-          animation: 1.5s ease-in jump;
-          overflow: auto;
+          font-size: 16px;
         }
 
-
+        &:hover {
+          svg {
+            animation: 1.5s ease-in jump;
+            overflow: auto;
+          }
+        }
       }
     }
   }
-}
 
-.experience {
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  opacity: 0;
-  transition: 2.5s;
-  border-bottom: 1px solid black;
-
-  .experienceItem {
-    width: 100%;
-    flex-grow: 0.6;
+  .experience {
+    min-height: 100vh;
     display: flex;
-    justify-content: flex-start;
-
-    .education,
-    .workExperience {
-      width: 30%;
-
-      .title {
-        color: rgb(120, 120, 120);
-        display: block;
-        margin-bottom: 40px;
-      }
-
-      h3 {
-        font-size: 30px;
-        margin-bottom: 8px;
-      }
-
-      .info {
-        color: rgb(98, 97, 97);
-        font-size: 16px;
-      }
-    }
-  }
-}
-
-.project {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  height: 100vh;
-  width: 100%;
-  opacity: 0;
-  transition: 2.5s;
-  border-bottom: 1px solid black;
-
-  .showProjects {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-
-    .projectItem {
-      width: 30%;
-      height: 400px;
-      cursor: pointer;
-      border: 1px solid gray;
-      display: flex;
-      justify-content: center;
-      flex-direction: column;
-      align-items: center;
-
-      img {
-        width: 80%;
-      }
-
-      p {
-        text-align: center;
-        font-size: 24px;
-        margin-bottom: 8px;
-      }
-    }
-  }
-}
-
-.whereIAm {
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  opacity: 0;
-  transition: 2.5s;
-  border-bottom: 1px solid black;
-
-  .social {
-
-    width: 80%;
-    font-size: 56px;
+    flex-direction: column;
     justify-content: space-around;
-    display: flex;
+    align-items: center;
+    opacity: 0;
+    transition: 2.5s;
+    border-bottom: 1px solid black;
 
-    .socialItem {
-      cursor: pointer;
+    .experienceItem {
+      width: 100%;
+      flex-grow: 0.6;
+      display: flex;
+      justify-content: flex-start;
 
-      &:hover {
-        animation: jump 1s;
+      @media (max-width: 576px) {
+        justify-content: space-around;
+      }
+      .education,
+      .workExperience {
+        width: 30%;
+        @media (max-width: 576px) {
+          width: 40%;
+        }
+        .title {
+          color: rgb(120, 120, 120);
+          display: block;
+          margin-bottom: 40px;
+        }
+
+        h3 {
+          font-size: 30px;
+          margin-bottom: 8px;
+        }
+
+        .info {
+          color: rgb(98, 97, 97);
+          font-size: 16px;
+        }
       }
     }
   }
-}
 
-footer {
-  text-align: center;
-  margin: 8px 0;
-  font-size: 6px;
-}
+  .project {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    min-height: 100vh;
+    width: 100%;
+    opacity: 0;
+    transition: 2.5s;
+    border-bottom: 1px solid black;
 
-.appear {
-  opacity: 1;
-}
+    .showProjects {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      .projectItem {
+        width: 30%;
+        height: 400px;
+        cursor: pointer;
+        border: 1px solid gray;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        @media (max-width: 576px) {
+          width: 100%;
+          margin-bottom: 30px;
+        }
+        img {
+          width: 80%;
+        }
 
-@keyframes jump {
-  0% {
-    transform: translateY(-16px);
+        p {
+          text-align: center;
+          font-size: 24px;
+          margin-bottom: 8px;
+        }
+      }
+    }
   }
 
-  20% {
-    transform: translateY(0);
+  .whereIAm {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    opacity: 0;
+    transition: 2.5s;
+    border-bottom: 1px solid black;
+    @media (max-width: 576px) {
+      justify-content: space-evenly;
+    }
+    .social {
+      width: 80%;
+      font-size: 56px;
+      justify-content: space-around;
+      display: flex;
+      @media (max-width: 576px) {
+        font-size: 36px;
+      }
+      .socialItem {
+        cursor: pointer;
+
+        &:hover {
+          animation: jump 1s;
+        }
+      }
+    }
   }
 
-  40% {
-    transform: translateY(-8px);
+  footer {
+    text-align: center;
+    margin: 16px 0;
+    font-size: 6px;
   }
 
-  60% {
-    transform: translateY(0);
+  .appear {
+    opacity: 1;
   }
 
-  80% {
-    transform: translateY(-2px);
-  }
+  @keyframes jump {
+    0% {
+      transform: translateY(-16px);
+    }
 
-  100% {
-    transform: translateY(0);
+    20% {
+      transform: translateY(0);
+    }
+
+    40% {
+      transform: translateY(-8px);
+    }
+
+    60% {
+      transform: translateY(0);
+    }
+
+    80% {
+      transform: translateY(-2px);
+    }
+
+    100% {
+      transform: translateY(0);
+    }
   }
-}
 </style>
