9f6d86c2247ddd40b3b830b7f2c4039a145424bb
1446450047 <1446450047@qq.com>
2022-12-08 22:04:12 +0800

    feat: 添加手机端侧边栏出现消失

--- src/pages/blogsView/components/blogContent/blogContent.vue
+++ src/pages/blogsView/components/blogContent/blogContent.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="wrapper">
-    <div class="menu">
+    <div :class="`menu ${menuVisible && 'display'}`">
+      <div class="closeMenu" @click="menuVisible = false">x</div>
       <p v-for="item in Object.values(blogMenu)">
-        <router-link :to="item.url">
+        <router-link @click="menuVisible = false" :to="item.url">
           {{ item.title }}
         </router-link>
       </p>
     </div>
-    <div class="content">
+    <div class="content" @click="() => (menuVisible = false)">
       <h1 class="title">{{ blog.title }}</h1>
       <p>
         <span v-for="item in blog.tags"
@@ -17,16 +18,24 @@
       <p>{{ blog.time }}</p>
       <div v-html="blog.component"></div>
     </div>
+    <div class="switch" @click="openMenu">
+      <Svg class="rightArrow"></Svg>
+    </div>
   </div>
 </template>
 <script setup>
   import Prism from "prismjs";
   import { useRoute } from "vue-router";
-  import { onMounted } from "vue";
+  import { onMounted, ref } from "vue";
   import blogMenu from "@/blogs/blogMenu";
   import Orange from "@/pages/cv/components/orange.vue";
+  import Svg from "@/components/svg/svg.vue";
   const route = useRoute();
   const blog = blogMenu[route.params.blogview];
+  const menuVisible = ref(false);
+  const openMenu = () => {
+    menuVisible.value = !menuVisible.value;
+  };
   onMounted(() => {
     Prism.highlightAll();
   });
@@ -40,8 +49,10 @@
     padding-top: 20px;
     .menu {
       width: 18%;
+      transition: 0.5s;
       @media (max-width: 576px) {
-        padding-top: 25px;
+        transform: translateX(-100%);
+        padding-top: 125px;
         padding-left: 25px;
         position: fixed;
         width: 70%;
@@ -49,8 +60,10 @@
         left: 0;
         top: 0;
         min-height: 100vh;
-        border: 1px solid red;
         background-color: #fff;
+        &.display {
+          transform: translateX(0);
+        }
       }
       p {
         margin-bottom: 24px;
@@ -59,6 +72,13 @@
         white-space: nowrap;
         text-overflow: ellipsis;
         word-break: keep-all;
+      }
+      .closeMenu {
+        position: relative;
+        top: -60px;
+        display: inline;
+        left: 70%;
+        font-size: 40px;
       }
     }
     .content {
@@ -82,6 +102,19 @@
       :deep(*) {
         margin: revert;
       }
+    }
+    .switch {
+      position: fixed;
+      left: 5px;
+      top: 50%;
+      width: 50px;
+      height: 50px;
+      font-size: 50px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: #fff;
+      border-radius: 50%;
     }
     @media (max-width: 576px) {
       padding: 0;
