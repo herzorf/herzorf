187fe970e9aa931cbba3095cc1973b1707bf6d45
herzorf <1446450047@qq.com>
2023-01-29 21:54:09 +0800

    更新简历的预览链接

--- src/pages/cv/components/other-link.vue
+++ src/pages/cv/components/other-link.vue
@@ -1,23 +1,27 @@
 <template>
-    <section>
-        <MainTitle>其他链接</MainTitle>
-        <p><a href="https://www.zhihu.com/people/hezhongfeng/posts">我的博客</a>：里面记录了学习时总结的一些文章</p>
-        <p><a href="https://github.com/herzorf">我的Github主页</a>：我的代码仓库</p>
-        <p><a href="http://101.34.69.179/#/cv">这个简历的在线链接</a>：http://101.34.69.179/#/cv</p>
-    </section>
+  <section>
+    <MainTitle>其他链接</MainTitle>
+    <p>
+      <a href="https://www.zhihu.com/people/hezhongfeng/posts">我的博客</a
+      >：里面记录了学习时总结的一些文章
+    </p>
+    <p><a href="https://github.com/herzorf">我的Github主页</a>：我的代码仓库</p>
+    <p>
+      <a href="http://101.34.69.179/#/cv">这个简历的在线链接</a
+      >：herzorf.info/#/cv
+    </p>
+  </section>
 </template>
 <script setup lang="ts">
-import MainTitle from './main-title.vue';
-
-
+  import MainTitle from "./main-title.vue";
 </script>
 <style lang="scss" scoped>
-section {
+  section {
     margin-top: 12px;
 
     p {
-        margin-top: 1em;
-        display: flex;
+      margin-top: 1em;
+      display: flex;
     }
-}
+  }
 </style>
